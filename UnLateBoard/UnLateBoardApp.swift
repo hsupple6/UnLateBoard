c5da8e9cd73f7d1bcd7bcf27de1482460b670c4e
hsupple <hsupple06@gmail.com>
2025-04-26 18:30:32 -0400

    Initial Commit

---
+++ UnLateBoard/UnLateBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnLateBoardApp.swift
+//  UnLateBoard
+//
+//  Created by Hayden Supple on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct UnLateBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnLateBoardTests/UnLateBoardTests.swift
@@ -0,0 +1,17 @@
+//
+//  UnLateBoardTests.swift
+//  UnLateBoardTests
+//
+//  Created by Hayden Supple on 4/26/25.
+//
+
+import Testing
+@testable import UnLateBoard
+
+struct UnLateBoardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
